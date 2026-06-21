f7fa4cc9b541393fbb1a2c929953e1d076fa7fbd
Ali Hamza <alihamzaahmad680@gmail.com>
2026-06-21 18:09:13 +0500

    change the files

--- middlewares/authUser.js
+++ middlewares/authUser.js
@@ -1,28 +1,3 @@
-// import jwt from "jsonwebtoken";
-
-// const authUser = async (req, res, next) => {
-//   const { token } = req.cookies;
-
-//   if (!token) {
-//     return res.json({ success: false, message: "Not Authorized" });
-//   }
-
-//   try {
-//     const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
-
-//     if (tokenDecode && tokenDecode.id) {
-//       req.userId = tokenDecode.id;
-//       next();
-//     } else {
-//       return res.json({ success: false, message: "Not Authorized" });
-//     }
-
-//   } catch (error) {
-//     return res.json({ success: false, message: error.message });
-//   }
-// };
-
-// export default authUser;
 import jwt from "jsonwebtoken";
 
 const authUser = async (req, res, next) => {
